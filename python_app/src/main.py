821198c9a8ab699573348370dad702beced8a5c4
Tuan Vu <tuan.vu@palexy.com>
2024-08-01 22:56:19 +0700

    add python app

---
+++ python_app/src/main.py
@@ -0,0 +1,12 @@
+import sys
+
+import numpy as np
+
+
+if __name__ == '__main__':
+    print(sys.path)
+    print(sys.executable)
+    print(sys.version_info)
+    arr = np.array([1, 2, 3])
+    print(arr)
+
